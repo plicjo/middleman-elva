4a1adab9a91ac7fd445cdb4e076ae1d008db4972
Joshua Plicque <plicjo@gmail.com>
2016-01-28 01:16:20 -0500

    Update config

--- config.rb
+++ config.rb
@@ -1,75 +1,23 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
 # Automatic image dimensions on image_tag helper
- activate :automatic_image_sizes
+activate :automatic_image_sizes
 # Index pages inside directories will be that directory's home page
- activate :directory_indexes
+activate :directory_indexes
 
 # Reload the browser automatically whenever files change
  configure :development do
    activate :livereload
  end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-   activate :minify_javascript
-
-  # Enable cache buster [Disable this if this will be migrated to Wordpress/non-static]
-   activate :asset_hash
-
-  # Use relative URLs
-   activate :relative_assets
-
-  # Favicon and Apple Touch Icon Generator
+  activate :asset_hash
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
     activate :favicon_maker, :icons => {
         "favicon_template.png" => [
           { icon: "apple-touch-icon-152x152-precomposed.png" },
@@ -81,13 +29,9 @@ configure :build do
         ]
     }
 
-  # Image Optimization
-   activate :imageoptim
-
   # Gzip Compression
-   # activate :gzip
+  # activate :gzip
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
-
 end
